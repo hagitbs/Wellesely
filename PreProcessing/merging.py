8b55616eacf658e0c3d62e4507c6b05a5f3a058f
hagitbs <hagits@gmail.com>
2023-08-24 22:42:18 +0300

    d

--- PreProcessing/merging.py
+++ PreProcessing/merging.py
@@ -13,7 +13,7 @@ print(big_merged_df )
 print (big_merged_df.columns)
  
 big_merged_df.rename(columns={'word': 'word', 'Maxpos_x': 'POS','wordCount_x':'wordCount-Global', 'percent_of_total_x':'percent_of_total-Global',
-                                              'Maxpos_y': 'POS-Chat','wordCount_x':'wordCount-Chat', 'percent_of_total_x':'percent_of_total-Chat', 
+                                              'Maxpos_y': 'POS-Chat','wordCount_y':'wordCount-Chat', 'percent_of_total_y':'percent_of_total-Chat', 
                                               'Maxpos':'POS-Human', 'wordCount':'wordCount-Human', 'percent_of_total':'percent_of_total-Human'}, inplace=True)
 
 big_merged_df.to_csv('agg_merged_all.csv',  index=False)
